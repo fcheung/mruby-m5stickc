d28d7f44c02d4234bdda1ba9fdb867cd1590db71
Frederick Cheung <frederick.cheung@gmail.com>
2019-12-26 23:46:12 +0000

    fix indentation in makefile

--- components/mruby_component/component.mk
+++ components/mruby_component/component.mk
@@ -7,9 +7,9 @@ COMPONENT_ADD_INCLUDEDIRS := mruby/include/
 CFLAGS += -Wno-char-subscripts -Wno-pointer-sign
 
 build: 
-  cd $(COMPONENT_PATH)/mruby && MRUBY_CONFIG=../esp32_build_config.rb $(MAKE)
-  cp $(COMPONENT_PATH)/mruby/build/esp32/lib/libmruby.a $(COMPONENT_LIBRARY)
+	cd $(COMPONENT_PATH)/mruby && MRUBY_CONFIG=../esp32_build_config.rb $(MAKE)
+	cp $(COMPONENT_PATH)/mruby/build/esp32/lib/libmruby.a $(COMPONENT_LIBRARY)
 
 clean: 
-  cd $(COMPONENT_PATH)/mruby && MRUBY_CONFIG=../esp32_build_config.rb $(MAKE) clean
+	cd $(COMPONENT_PATH)/mruby && MRUBY_CONFIG=../esp32_build_config.rb $(MAKE) clean
 
