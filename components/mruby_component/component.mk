1b9c204c2642a25c22c8abb725ddca8ae08628ed
Frederick Cheung <frederick.cheung@gmail.com>
2020-01-07 11:21:47 +0000

    biuld mruby with -j4

--- components/mruby_component/component.mk
+++ components/mruby_component/component.mk
@@ -7,7 +7,7 @@ COMPONENT_ADD_INCLUDEDIRS := mruby/include/
 CFLAGS += -Wno-char-subscripts -Wno-pointer-sign
 
 build: 
-	cd $(COMPONENT_PATH)/mruby && MRUBY_CONFIG=../esp32_build_config.rb $(MAKE)
+	cd $(COMPONENT_PATH)/mruby && MRUBY_CONFIG=../esp32_build_config.rb ruby ./minirake -j4
 	cp $(COMPONENT_PATH)/mruby/build/esp32/lib/libmruby.a $(COMPONENT_LIBRARY)
 
 clean: 
